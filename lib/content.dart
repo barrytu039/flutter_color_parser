2fba160ba13fee4c1ea44028baa53a542ca81c28
barry <airagtio09@gmail.com>
2021-04-05 23:16:49 +0800

    first commit

---
+++ lib/content.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+
+class ContentWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ColorStateWidget();
+}
+
+class ColorStateWidget extends State<ContentWidget> {
+
+  Future<List<ColorModel>> futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    futureData = fetchAlbum();
+  }
+
+  final List<ColorModel> listItems = new List<ColorModel>.generate(500, (i) {
+    return ColorModel(
+      url: 'https://xxxxxx. $i',
+      title: 'Name：$i',
+    );
+  });
+
+  Future<List<ColorModel>> fetchAlbum() async {
+    final response = await http.get(Uri.https('jsonplaceholder.typicode.com', 'photos'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      final List<ColorModel> data = [];
+      final List<dynamic> fetchData = jsonDecode(response.body);
+      for (var i = 0; i < fetchData.length; i++) {
+        final ColorModel model = ColorModel(
+            title: fetchData[i]['title'],
+            url: fetchData[i]['url']);
+        data.add(model);
+      }
+      return data;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Content Page"),
+        ),
+        body: Center(
+          child: FutureBuilder<List<ColorModel>> (
+            future: futureData,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Center(
+                    child: new GridView.builder(gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3, //每行三列
+                        childAspectRatio: 1.0 //显示区域宽高相等
+                    ),
+                        itemCount: listItems.length,
+                        itemBuilder: (context, index) {
+                          return
+                            new Image.network(snapshot.data[index].url);
+                        }
+                    )
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            }
+          )
+        )
+      );
+    }
+  }
+
+class ColorModel {
+  final String url;
+  final String title;
+
+  ColorModel({this.url, this.title});
+
+  factory ColorModel.fromJson(Map<String, dynamic> json) {
+    return ColorModel(
+      url: json['url'],
+      title: json['title'],
+    );
+  }
+}
+
+
+
